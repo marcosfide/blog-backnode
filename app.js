1e307de1e1d62544faff0333bf0d7560632bed29
Marcos Fidelibus <fidelibusmarcosgmail.com>
2025-01-06 20:28:34 +0100

    modificando port

--- app.js
+++ app.js
@@ -12,10 +12,14 @@ app.use(cors());
 app.use(express.json());
 app.use('/blogs', blogRoutes);
 
+// Ruta base
 app.get('/', (req, res) => {
     res.send('Hola Mundo');
 });
 
-app.listen(8000, () => {
-    console.log('Server UP running in http://localhost:8000/');
+// Usar el puerto definido por Railway o un valor por defecto para desarrollo local
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server UP running on port ${PORT}`);
 });
