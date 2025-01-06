f08b941c84e6744e45dfd423d8b770aed1d8e985
Marcos Fidelibus <fidelibusmarcosgmail.com>
2025-01-06 19:55:59 +0100

    modificando db.js

--- database/db.js
+++ database/db.js
@@ -5,11 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    // Conectar a MongoDB usando Mongoose
-    const conn = await mongoose.connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.DB_URI); // Sin opciones
     console.log(`MongoDB conectado: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error al conectar a MongoDB: ${error.message}`);
